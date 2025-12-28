0121ac4553d66b67dfe06560c534ffeb9930dc5b
Tiago223F <tiagompf22@gmail.com>
2025-12-28 20:51:59 +0000

    main formato

--- calculator.py
+++ calculator.py
@@ -1,15 +1,13 @@
 from ler_dados import ler_dados
 
-def calculate():
+def somatorio(): #somatorio do saldo total
     dados = ler_dados()
     total_positivo = 0
     total_negativo = 0
 
-    # Usamos .items() para "desembrulhar" os níveis
     for tipo, categorias in dados.items():
         for categoria, subcategorias in categorias.items():
             for subcategoria, lista_gastos in subcategorias.items():
-                # lista_gastos é a lista de tuplas [(valor, data), ...]
                 for valor, data in lista_gastos:
                     if tipo == '+':
                         total_positivo += valor
@@ -19,8 +17,32 @@ def calculate():
     saldo_final = total_positivo - total_negativo
     return total_positivo, total_negativo, saldo_final
 
+def soma_mes(mes): #somatorio do saldo mensal
+    dados = ler_dados()
+    positivo_mes = 0
+    negativo_mes = 0
+
+    for tipo, categorias in dados.items():
+        for categoria, subcategorias in categorias.items():
+            for subcategoria, lista_gastos in subcategorias.items():
+                for valor, data in lista_gastos:
+                    num_mes=data.split('/')
+                    if tipo == '+' and mes==num_mes[1]:
+                        positivo_mes += valor
+                    elif tipo == '-' and mes==num_mes[1]:
+                        negativo_mes += valor
+    
+    saldo_mes = positivo_mes - negativo_mes
+    return positivo_mes, negativo_mes, saldo_mes
+
+
+
+
+
+
 if __name__ == "__main__":
-    positivo, negativo, saldo = calculate()
-    print(f"Total Positivo: {positivo}")
-    print(f"Total Negativo: {negativo}")
+    positivo, negativo, saldo = soma_mes('11')
+    print(f"Total Positivo: {positivo:.2f}")
+    print(f"Total Negativo: {negativo:.2f}")
     print(f"Saldo Final: {saldo:.2f}")
+    
--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+from calculator import soma_mes
+
+
 print('='*50)
-print("=== Welcome to Financial Python Program ===")
+print("======= Welcome to Financial Python Program ======")
 print('='*50)
-dados= ler_dados()
+print("Here is where you can manage your finances effectively.")
+print('='*50)
+while True:
+    output=input('What operation would you like to perform? ')
+    print('1.Budget')
+    print('2.Investments')
+    print('3.Casino')
+    print('4.')
+    print('5.EXIT')
+    try:
+        numero = int(output)
+        if numero==1:
+            pass
+        if numero==2:
+            pass
+        if numero==3:
+            pass
+        if numero==4:
+            pass
+        if numero==5:
+            print('='*50)
+            print("======== CLOSING Financial Python Program ========")
+            print('='*50)
+            break
+    except (ValueError, TypeError):
+        print('='*50)
+        print("Erro: Please insire a operation number(1-5).")
+        print('='*50)
+        
+    
+
+    
